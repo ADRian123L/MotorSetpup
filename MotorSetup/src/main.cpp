1d74e5bdd060c5664817863efc764aa83a093401
jiahui0926 <jiahuidang@usf.edu>
2022-12-11 17:16:29 -0500

    Added the code for mega

--- MotorSetup/src/main.cpp
+++ MotorSetup/src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h> 
+#include <Arduino.h>
 
 // Struct:
 struct MotorAttribute {
@@ -19,15 +19,15 @@ struct MotorAttribute Motor1,
 
 
 // PWD pins constants:
-const int PWD1 = 3, PWD2 = 5, 
-          PWD4 = 11, PWD3 = 6;
+const int PWD1 = 2, PWD2 = 3, 
+          PWD4 = 4, PWD3 = 5;
 const int MAX = 255, MIN = 0;
 
 // Direction pins:
-const int PIN1R = 4, PIN1L = 7,
-          PIN2R = 13, PIN2L = 12,
-          PIN3R = 8, PIN3L = 9,
-          PIN4R = 10, PIN4L = 2;
+const int PIN1R = 30, PIN1L = 31,
+          PIN2R = 32, PIN2L = 33,
+          PIN3R = 34, PIN3L = 35,
+          PIN4R = 36, PIN4L = 37;
 
 // Functions:
 int degree(void);
@@ -81,6 +81,8 @@ void setup() {
 
 void loop() {
 
+
+Serial.println("it works");
   // Empty var:
   int input, return1, return2, return3, return4;
   double radian_value, Xcomp, Ycomp;
