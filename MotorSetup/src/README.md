68a8ef280572a2cdd0717b6953e06a0372ed4f18
jiahui0926 <jiahuidang@usf.edu>
2022-12-12 23:59:38 -0500

    Finished version 2

--- MotorSetup/src/README.md
+++ MotorSetup/src/README.md
@@ -3,27 +3,27 @@ This file contains the program's documentation.
 Pins numbers:
     Motor_1:
         Pbw = 3
-        direction pins:
+        Direction pins:
             counterclockwise = 4
             clockwise = 7
 
 Pins numbers:
     Motor_2:
-        Pbw = 6
-        direction pins:
+        Pbw = 5
+        Direction pins:
             counterclockwise = 13
             clockwise = 12
 
 Pins numbers:
     Motor_3:
         Pbw = 6
-        direction pins:
+        Direction pins:
             counterclockwise = 9
             clockwise = 8
 
 Pins numbers:
     Motor_4:
         Pbw = 11
-        direction pins:
+        Direction pins:
             counterclockwise = 2
             clockwise = 10
--- MotorSetup/src/main.cpp
+++ MotorSetup/src/main.cpp
@@ -1,65 +1,74 @@
 #include <Arduino.h>
+
 // Struct:
 struct MotorAttribute {
+  // Pins attribute:
   int pin_1,
       pin_2,
       pin_pbw;
-      
-  
-   double   velocity,
-            power_supply;
+  // Speed attribute:
+   double  velocity,
+          power_supply;
 
 };
 
-// Struct:
+// Initialize four objects:
 struct MotorAttribute Motor1, 
                       Motor2,
                       Motor3,
                       Motor4;
 
-
-
 // PWD pins constants:
 const int PWD1 = 3, PWD2 = 5, 
-          PWD4 = 6, PWD3 = 11;
-const int MAX = 255, MIN = 0;
+          PWD3 = 6, PWD4 = 11;
+// Max speed:
+const int MAX = 255;
+// Varying speed:
+int current_speed;
+
 
 // Direction pins:
-const int PIN1R = 12, PIN1L = 13,
-          PIN2R = 7, PIN2L = 4,
-          PIN3R = 8, PIN3L = 9,
+const int PIN1R = 7, PIN1L = 4,
+          PIN2R = 12, PIN2L = 13,
+          PIN3R = 9, PIN3L = 8,
           PIN4R = 2, PIN4L = 10;
 
+// Count variable:
+int count = 0;
+
 // Functions:
-int degree(void);
+int input_degree(void);
+double input_speed(void);
 double converter(int degree_arg);
 double x_components(double radian_x);
 double y_components(double radian_y);
 void motors(double xcomp, double ycomp);
-void set_power(MotorAttribute wheel_name);
-double motor_control(MotorAttribute motor_name);
+void motor_control(MotorAttribute motor_name);
+void speed(double percentage);
 
 // String Constants:
-const String prompt = "Add the angle: ";
+const String prompt = "Add the angle: ",
+             prompt2 = "Add speed level: ";
 
 void setup() {
   
   //Assign each object its pins:
-  Motor2.pin_1 = PIN1L;
-  Motor2.pin_2 = PIN1R;
-  Motor2.pin_pbw = PWD1;
+  Motor1.pin_1 = PIN1L;
+  Motor1.pin_2 = PIN1R;
+  Motor1.pin_pbw = PWD1;
 
-  Motor1.pin_1 = PIN2L;
-  Motor1.pin_2 = PIN2R;
-  Motor1.pin_pbw = PWD2;
+  Motor2.pin_1 = PIN2L;
+  Motor2.pin_2 = PIN2R;
+  Motor2.pin_pbw = PWD2;
 
-  Motor4.pin_1 = PIN3L;
-  Motor4.pin_2 = PIN3R;
-  Motor4.pin_pbw = PWD3;
+  Motor3.pin_1 = PIN3L;
+  Motor3.pin_2 = PIN3R;
+  Motor3.pin_pbw = PWD3;
 
-  Motor3.pin_1 = PIN4L;
-  Motor3.pin_2 = PIN4R;
-  Motor3.pin_pbw = PWD4;
+  Motor4.pin_1 = PIN4L;
+  Motor4.pin_2 = PIN4R;
+  Motor4.pin_pbw = PWD4;
+
 
  // Pins type:
   pinMode(PWD1, OUTPUT);
@@ -82,48 +91,43 @@ void setup() {
 
 void loop() {
 
-  Serial.println("it works");
   // Empty var:
-  int input, return1, return2, return3, return4;
+  int degree_input, speed_percent;
   float radian_value;
-  double Xcomp, Ycomp;
+  double X_comp, Y_comp;
 
   // Prompts for degrees:
-  input = 45;
+  degree_input = 90;
+  speed_percent = 50;
+
+  // Prints the motors' properties:
+  Serial.print("Angle: ");
+  Serial.println(count);
+
+  // Sets the speed:
+  speed(speed_percent);
 
   // Converts to radians:
-  radian_value = converter(input);
+  radian_value = converter(degree_input);
 
   // Converts into (x - y) components:
-  Xcomp = x_components(radian_value);
-  Ycomp = y_components(radian_value);
+  X_comp = x_components(radian_value);
+  Y_comp = y_components(radian_value);
   
-
   // Calculates the power and direction:
-  motors(Xcomp, Ycomp);
+  motors(X_comp, Y_comp);
+  motor_control(Motor1);
+  motor_control(Motor2);
+  motor_control(Motor3);
+  motor_control(Motor4);
 
-  // Set motors' output:
-  return1 = motor_control(Motor1);
-  return2 = motor_control(Motor2);
-  return3 = motor_control(Motor3);
-  return4 = motor_control(Motor4);
+  Serial.println(MAX);
 
-  // Prints:
-  Serial.println(radian_value);
-  Serial.println(Xcomp);
-  Serial.println(Ycomp);
-  Serial.println(return1);
-  Serial.println(return2);
-  Serial.println(return3);
-  Serial.println(return4);
-  
-  Serial.println(Motor1.velocity);
-  delay(9000000);
+  delay(4000000);
 }
 
-
-// Read the input in degrees:
-int degree(void) {
+// Read the degree_input in degrees:
+int input_degree(void) {
   // Empty variable:
   int get_degree;
   //Prints the prompt:
@@ -137,10 +141,25 @@ int degree(void) {
    return get_degree;
 }
 
+// Read the degree_input in speed:
+double input_speed(void) {
+  // Empty variable:
+  double get_speed;
+  //Prints the prompt:
+  Serial.println(prompt2);
+  // Waits until a message is sent:
+  while (Serial.available() == 0) {
+    // Delays.
+  }
+  // Saves the message:
+  get_speed = Serial.parseInt();
+  return get_speed;
+}
+
 // Convert to radians: 
 double converter(int degree_arg) {
   double radians;
-  int new_degree = degree_arg + 45;
+  double new_degree = degree_arg + 45;
   // Converts to radians:
   radians =  new_degree * (PI / 180);
   return radians;
@@ -148,39 +167,22 @@ double converter(int degree_arg) {
 
 // Converts the radians into its x-component:
 double x_components(double radian_x) {
-
-    // Empty variables:
-    
-    double value_x = cos(radian_x);
-
     // Finds the component:
-    
-    Serial.print("it is");
-    Serial.println(value_x);
-    
-
+    double value_x = cos(radian_x);
     return value_x;
 }
 
 // Converts the radians into its y-component:
 double y_components(double radian_y) {
-
-    // Empty variables:
-    
+    // Empty variables:    
     double value_y;
-
     // Finds the component:
-
     value_y = sin(radian_y);
-
-
     return value_y;
 }
 
 // Control the motors:
 void motors(double xcomp, double ycomp) {
-  // Empty int variable:
- 
   // Assign the attributes:
   Motor1.velocity = 0 - xcomp;
   Motor2.velocity = ycomp;
@@ -188,38 +190,36 @@ void motors(double xcomp, double ycomp) {
   Motor4.velocity = ycomp;
 }
 
-void set_power(MotorAttribute wheel_name) {
-  
-  wheel_name.power_supply = abs(wheel_name.velocity * 255);
-
-}
-
-double motor_control(MotorAttribute motor_name) {
-
-    motor_name.power_supply = abs(motor_name.velocity * 255);
-    
+// Drives the motors:
+void motor_control(MotorAttribute motor_name) {
+    // Assigns the power to the object:
+    motor_name.power_supply = abs(motor_name.velocity * current_speed);
+    // Runs the motors:
     if (motor_name.velocity > 0) {
-
-    
       digitalWrite(motor_name.pin_1, HIGH);
       digitalWrite(motor_name.pin_2, LOW);
       analogWrite(motor_name.pin_pbw, motor_name.power_supply);
-      return motor_name.power_supply;
     }
     else if (motor_name.velocity == 0) {
       digitalWrite(motor_name.pin_1, LOW);
       digitalWrite(motor_name.pin_2, LOW);
-      analogWrite(motor_name.pin_pbw, motor_name.power_supply);
-      return motor_name.power_supply; 
+      analogWrite(motor_name.pin_pbw, motor_name.power_supply); 
     }
     else {
       digitalWrite(motor_name.pin_1, LOW);
       digitalWrite(motor_name.pin_2, HIGH);
       analogWrite(motor_name.pin_pbw, motor_name.power_supply);
-      return motor_name.power_supply;
-
     }
-
-    return -1;
-
+    Serial.println(motor_name.velocity);
   }
+  
+void speed(double percentage) {
+  // Empty var:
+  double decimal;
+  // Converts to percentage:
+  decimal = percentage / 100;
+  // Assigns the value:
+  current_speed = round(decimal * MAX);
+  Serial.print("Speed: ");
+  Serial.println(current_speed);
+}
