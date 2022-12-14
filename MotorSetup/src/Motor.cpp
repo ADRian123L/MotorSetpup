3f10ed14315cf5427cbbe17cf90d364d8ed8d088
ADRian123L <adrian163@usf.edu>
2022-12-13 23:12:04 -0500

    Added a description.

--- MotorSetup/src/Motor.cpp
+++ MotorSetup/src/Motor.cpp
@@ -1,6 +1,17 @@
-class Motor{
+/*
+    The program is an object meant to control a vehicle.
+    The object can fully control a vehicle (wheels, encoders, speed, and 
+    direction)
+    When the object is initiated it asks for the pins, and the wheel's orientation.
+    Then with a method the vehicle's speed and direction can be changed.
+*/
+
+class Motor {
+
 private:
+
 public:
+
     double velocity;
     
 
